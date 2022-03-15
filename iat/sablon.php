b1b687894648690fff6a5532e0401041a4ed14e6
Mavera43 <99175370+Mavera43@users.noreply.github.com>
2022-03-15 10:50:35 +0300

    Update sablon.php

--- iat/sablon.php
+++ iat/sablon.php
@@ -1,25 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <title>Php Sabitler, Değişkenler, Karar yapıları</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
+
 <body>
-<form action="/action_page.php">
-  <div class="form-group">
-    <label for="email">Email address:</label>
-    <input type="email" class="form-control" id="email">
+  <div class="container ">
+    <h2> </h2>
+    <div class="card">
+      <div class="card-header bg-danger">
+        Header
+      </div>
+      <div class="card-body bg-secondary">
+       
+    </div>
+    <div class="card-footer bg-danger">
+        Footer
+      </div>
   </div>
-  <div class="form-group">
-    <label for="pwd">Password:</label>
-    <input type="password" class="form-control" id="pwd">
-  </div>
-  <div class="checkbox">
-    <label><input type="checkbox"> Remember me</label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
+  <br>
+  <br>
+  <br>
+  <br>
+  
 </body>
+
 </html>
